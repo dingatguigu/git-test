cecfea1078070dc74b8dcf57657a336df0ce11ea
Ding <ding@atguigu.com>
2022-01-16 15:56:10 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,16 @@
+package com.atguigu.git;
+
+/**
+ * @author dwdstart
+ * @create 2022-01-16-15:51
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+
+        System.out.println("hello,git");
+
+    }
+
+
+}
